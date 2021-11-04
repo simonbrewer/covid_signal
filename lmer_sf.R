416f5b047ba39baaac3e61e547f676b7cb708bbc
Simon Brewer <brewersi@gmail.com>
2021-11-03 22:36:56 -0600

    Added output of stepwise proc and threshold check

--- lmer_sf.R
+++ lmer_sf.R
@@ -33,8 +33,7 @@ lmer_sf <- function(formula, data, idcol,
   E.df[idcol] <- attr(nb, "region.id")
   ## Create list of EVs for selection
   Evar <- as.vector(paste0("E", seq(1,nE))) 
-  print(Evar)
-  
+
   ## Merge full set of eigenvectors back with data
   data <- merge(data, E.df, by = idcol)
   
@@ -50,6 +49,10 @@ lmer_sf <- function(formula, data, idcol,
   target_z <- target_MI$statistic
   target_p <- target_MI$p.value
   
+  ## Sanity check if autocorrelation $p$ is lower than threshold
+  if (target_p > pthresh)
+    stop("Current model p-value is higher than threshold")
+  
   if (verbose)
     print(paste("Basis:", "z:", target_z, "p-val:", target_p))
   
@@ -58,6 +61,9 @@ lmer_sf <- function(formula, data, idcol,
     cl <- parallel::makeCluster(8)
     doParallel::registerDoParallel(cl)
   }
+  
+  ## Output
+  all_j <- all_E <- all_p <- all_I <- all_z <- NULL
   
   ## Outer loop
   while ((target_p < pthresh) & (length(Evar) > 0))  {
@@ -86,18 +92,26 @@ lmer_sf <- function(formula, data, idcol,
           moran.test(resid_lev2, sig.lw)$p.value
         }
     }
+    ## Results of stepwise (chosen on maximum $p$, maybe should be min I?)
     candidate_j <- which.max(list_p)
+    all_j <- c(all_j, candidate_j)
     candidate_E <- Evar[candidate_j]
+    all_E <- c(all_E, candidate_E)
     candidate_p <- max(list_p)
-    
+
     ## Update model basis with selected EV
     model_basis <- update(model_basis, as.formula(paste(". ~ . + ", candidate_E)))
+    
     ## Get updated residuals
     resid_lev2 <- ranef(model_basis)[[1]][,1]
+    
     ## Calculate Moran's I on new residuals
     target_MI <- moran.test(resid_lev2, sig.lw)
+    all_I <- c(all_I, candidate_I)
     target_z <- target_MI$statistic
+    all_z <- c(all_z, candidate_z)
     target_p <- target_MI$p.value
+    all_p <- c(all_p, candidate_p)
     ## Remove selected EV from list
     Evar <- Evar[-candidate_j]
     
@@ -107,10 +121,15 @@ lmer_sf <- function(formula, data, idcol,
     
   }
   
+  ## Shutdown clusters
   if (parallel) 
     parallel::stopCluster(cl)
 
-  return(list(model_basis = model_basis,
+  ## Dataframe of stepwise results
+  step_out = data.frame(all_j, all_E, all_I, all_z, all_p)
+  ## Output
+  return(list(step_out = step_out,
+              model_basis = model_basis,
               mi = target_MI))
   
 }
